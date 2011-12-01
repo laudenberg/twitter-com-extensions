83220616a316cbb2ca9017a0e505cdd5fd261d80
Ruediger Jussen <ruediger.jussen@googlemail.com>
2011-12-01 12:40:06 +0100

    - switched to #page-container for the starting div - search for #stream-items-id .last-new-tweet then, that enables extension on pages other than home-stream

--- new-tweets-click.js
+++ new-tweets-click.js
@@ -1,7 +1,7 @@
 $(function() {
-  $("#page-node-home").on("click", "#new-tweets-bar", function() {
+  $("#page-container").on("click", "#new-tweets-bar", function() {
     console.log("New Tweets Bar clicked.");
-    var last_new_tweet = $("#home-stream-manager .last-new-tweet");
+    var last_new_tweet = $("#stream-items-id .last-new-tweet");
     $(window).scrollTop(last_new_tweet.offset().top - $(window).height() + last_new_tweet.outerHeight());
   });
 });
